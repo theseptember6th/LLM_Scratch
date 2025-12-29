b0e96f6ae7af21cc086b66440fecd8ef305355e6
Kristal Shrestha <kristalshrestha@Kristals-MacBook-Air.local>
2025-12-29 13:35:48 +0545

    if name main

---
+++ test_main/program.py
@@ -0,0 +1,8 @@
+import calculator
+
+num1 = 10
+num2 = 5
+
+print(f"Using the calculator module: ")
+print(f"The sum of {num1} and {num2} is {calculator.add(num1,num2)}")
+print(f"The difference between {num1} and {num2} is:{calculator.subtract(num1,num2)}")
