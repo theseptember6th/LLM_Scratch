c405da9c6cea67ce5e6a2f7d454e6a3c21f5f89f
Kristal Shrestha <kristalshrestha@Kristals-MacBook-Air.local>
2026-03-10 07:44:38 +0545

    anthropic

--- Langchain/langchain-models-master/2.ChatModels/2_chatmodel_anthropic.py
+++ Langchain/langchain-models-master/2.ChatModels/2_chatmodel_anthropic.py
@@ -1,10 +1,12 @@
 from langchain_anthropic import ChatAnthropic
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+api_key = os.getenv("CLAUDE_API_KEY")
 
-model = ChatAnthropic(model='claude-3-5-sonnet-20241022')
+model = ChatAnthropic(model="claude-3-5-sonnet-20241022", api_key=api_key)
 
-result = model.invoke('What is the capital of India')
+result = model.invoke("What is the capital of India")
 
 print(result.content)
