d6815fadaa2a99cc4e8343dd857a93a08da772ba
Kristal Shrestha <kristalshrestha@Kristals-MacBook-Air.local>
2026-03-10 15:35:01 +0545

    follow this

---
+++ Langchain/2_langchain_prompts/3_typesOfMessages/3_works_chat_prompt_template.py
@@ -0,0 +1,19 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+# make tuples to make it work
+# chat_template = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", "You are a helpful {domain} expert"),
+#         ("human", "Explain in simple terms,what is {topic}"),
+#     ]
+# )
+# this also work (recommended)
+chat_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful {domain} expert"),
+        ("human", "Explain in simple terms,what is {topic}"),
+    ]
+)
+prompt = chat_template.invoke({"domain": "cricket", "topic": "Dusra"})
+
+print(prompt)
