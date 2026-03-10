ffb7783157131524d0d52dfe8281c39fe483a587
Kristal Shrestha <kristalshrestha@Kristals-MacBook-Air.local>
2026-03-10 07:42:05 +0545

    openai chat

--- Langchain/langchain-models-master/2.ChatModels/1_chatmodel_openai.py
+++ Langchain/langchain-models-master/2.ChatModels/1_chatmodel_openai.py
@@ -1,9 +1,12 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-
-model = ChatOpenAI(model="gpt-4", temperature=1.5, max_completion_tokens=10)
+api_key = os.getenv("OPENAI_API_KEY")
+model = ChatOpenAI(
+    model="gpt-4o-mini", temperature=1.5, max_completion_tokens=10, api_key=api_key
+)
 
 result = model.invoke("Write a 5 line poem on cricket")
 
