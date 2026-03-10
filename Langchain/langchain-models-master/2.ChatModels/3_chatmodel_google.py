0f19df96851f384b87f61a591de4c4b2b6bf8e67
Kristal Shrestha <kristalshrestha@Kristals-MacBook-Air.local>
2026-03-10 07:53:27 +0545

    google's gemini chatmodel

--- Langchain/langchain-models-master/2.ChatModels/3_chatmodel_google.py
+++ Langchain/langchain-models-master/2.ChatModels/3_chatmodel_google.py
@@ -1,10 +1,11 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+import os
+
 
 load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+model = ChatGoogleGenerativeAI(model="gemini-3.1-pro-preview", api_key=api_key)
 
-model = ChatGoogleGenerativeAI(model='gemini-1.5-pro')
-
-result = model.invoke('What is the capital of India')
-
+result = model.invoke("What is the capital of India?")
 print(result.content)
