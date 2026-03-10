62bc9a7c2458602cdba84731761d914167f88c68
Kristal Shrestha <kristalshrestha@Kristals-MacBook-Air.local>
2026-03-10 08:54:32 +0545

    locally models

--- Langchain/langchain-models-master/2.ChatModels/5_chatmodel_hf_local.py
+++ Langchain/langchain-models-master/2.ChatModels/5_chatmodel_hf_local.py
@@ -1,18 +1,18 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 import os
 
-os.environ['HF_HOME'] = 'D:/huggingface_cache'
+os.environ["HF_HOME"] = "/Users/kristalshrestha/Documents/Code/LLM_Scratch/models"
 
 llm = HuggingFacePipeline.from_model_id(
-    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
-    task='text-generation',
-    pipeline_kwargs=dict(
-        temperature=0.5,
-        max_new_tokens=100
-    )
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs={
+        "max_new_tokens": 100,
+        "temperature": 0.5,
+    },
 )
+
 model = ChatHuggingFace(llm=llm)
 
-result = model.invoke("What is the capital of India")
-
+result = model.invoke("What is the capital of India?")
 print(result.content)
