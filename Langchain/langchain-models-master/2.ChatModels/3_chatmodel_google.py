a3b27a2d9ec1c4abbc0b482a1095dbeac81d1a35
Kristal Shrestha <kristalshrestha@Kristals-MacBook-Air.local>
2026-03-14 07:54:42 +0545

    updated gemini

--- Langchain/langchain-models-master/2.ChatModels/3_chatmodel_google.py
+++ Langchain/langchain-models-master/2.ChatModels/3_chatmodel_google.py
@@ -4,8 +4,8 @@ import os
 
 
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-model = ChatGoogleGenerativeAI(model="gemini-3.1-pro-preview", api_key=api_key)
+api_key = os.getenv("GOOGLE_API_KEY")
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", api_key=api_key)
 
 result = model.invoke("What is the capital of India?")
 print(result.content)
