64c4caa713d44e809f1be2e50fbb319e8b8d61c2
Kristal Shrestha <kristalshrestha@Kristals-MacBook-Air.local>
2026-03-17 15:27:18 +0545

    python code splitting

--- Langchain/6_RAG/2_Text_splitters/3.1_python_code_splitting.py
+++ Langchain/6_RAG/2_Text_splitters/3.1_python_code_splitting.py
@@ -38,8 +38,24 @@ splitter = RecursiveCharacterTextSplitter.from_language(
 # perform splitting
 
 chunks = splitter.split_text(text)
-print(len(chunks))
+print(len(chunks))  # 2
 print(chunks)
-print(type(chunks))
+"""Output:
+['class Student:\n    def __init__(self, name, age, grade):\n        self.name = name\n        self.age = age\n        self.grade = grade  # Grade is a float (like 8.5 or 9.2)\n\n    def get_details(self):\n        return self.name"\n\n    def is_passing(self):\n        return self.grade >= 6.0', '# Example usage\nstudent1 = Student("Aarav", 20, 8.2)\nprint(student1.get_details())\n\nif student1.is_passing():\n    print("The student is passing.")\nelse:\n    print("The student is not passing.")']
+"""
+print(type(chunks))  # <class 'list'>
 print(chunks[0])
-print(type(chunks[0]))
+"""first chunk Output:
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade  # Grade is a float (like 8.5 or 9.2)
+
+    def get_details(self):
+        return self.name"
+
+    def is_passing(self):
+        return self.grade >= 6.0
+"""
+print(type(chunks[0]))  # <class 'str'>
