67aac811f1b7aede1dca2f9040e6b1b7874cd142
Kristal Shrestha <kristalshrestha@Kristals-MacBook-Air.local>
2026-03-21 18:48:30 +0545

    added chain

--- Langchain/2_langchain_prompts/backend/model/predict.py
+++ Langchain/2_langchain_prompts/backend/model/predict.py
@@ -13,40 +13,7 @@ def predict_output(user_input: dict):
             "temperature": 0.5,
         },
     )
-
-    # template
-    # template = PromptTemplate(
-    #     template=""" Please summarize the research paper titled "{paper_input}" with the following
-    # specifications:
-    # Explanation Style: {style_input}
-    # Explanation Length: {length_input}
-    # 1. Mathematical Details:
-    # - Include relevant mathematical equations if present in the paper.
-    # - Explain the mathematical concepts using simple, intuitive code snippets
-    # where applicable.
-    # 2. Analogies:
-    # - Use relatable analogies to simplify complex ideas.
-    # If certain information is not available in the paper, respond with: "Insufficient
-    # information available" instead of guessing.
-    # Ensure the summary is clear, accurate, and aligned with the provided style and
-    # length.
-    # """,
-    #     input_variables=["paper_input", "style_input", "length_input"],
-    #     validate_template=True,
-    # )
     template = load_prompt("prompt/template.json")
-    # fill the placeholders
-    # prompt = template.format(
-    #     paper_input=user_input["paper_input"],
-    #     style_input=user_input["style_input"],
-    #     length_input=user_input["length_input"],
-    # )
-    # model = ChatHuggingFace(llm=llm)
-
-    # result = model.invoke(prompt)
-
-    # return {"output": result.content}
-
     # using chain
     chain = template | llm
     result = chain.invoke(
