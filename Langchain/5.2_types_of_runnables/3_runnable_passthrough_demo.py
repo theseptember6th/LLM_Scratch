8a2e32567c5dd737cd93c3c23e884a7fd4e08dae
Kristal Shrestha <kristalshrestha@Kristals-MacBook-Air.local>
2026-03-23 22:00:48 +0545

    runnable passthrough demo

--- Langchain/5.2_types_of_runnables/3_runnable_passthrough_demo.py
+++
@@ -1,5 +0,0 @@
-from langchain_core.runnables import RunnablePassthrough
-
-passthrough = RunnablePassthrough()
-print(passthrough.invoke(2))  # o/p : 2
-print(passthrough.invoke({"name": "kristal"}))  # same dictionary as o/p
