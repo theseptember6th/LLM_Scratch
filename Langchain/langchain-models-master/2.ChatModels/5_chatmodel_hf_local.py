bb8d2c7a691d69bee0fe23388b023643b1113cd2
Kristal Shrestha <kristalshrestha@MacBook-Pro.local>
2026-08-28 16:28:10 +0545

    locally models

--- Langchain/langchain-models-master/2.ChatModels/5_chatmodel_hf_local.py
+++ Langchain/langchain-models-master/2.ChatModels/5_chatmodel_hf_local.py
@@ -1,7 +1,10 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 import os
 
+
+# Set HuggingFace cache directory
 os.environ["HF_HOME"] = "/Users/kristalshrestha/Documents/Code/LLM_Scratch/models"
+
 
 llm = HuggingFacePipeline.from_model_id(
     model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
@@ -12,7 +15,14 @@ llm = HuggingFacePipeline.from_model_id(
     },
 )
 
+
+# Create a Chat Model instance
+# Wrap llm with chat interface
 model = ChatHuggingFace(llm=llm)
 
+# Invoke with a prompt
 result = model.invoke("What is the capital of India?")
+
+# The result is an object; print the whole object first to see the metadata
+# To get just the answer, access the 'content' attribute
 print(result.content)
